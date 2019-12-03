322ef5bdeb02a00736539f92c68486bea1b01352
Will Fuqua <wafuqua@gmail.com>
2019-12-03 23:18:34 +0700

    remove unnecessary dlls to improve startup perf

--- Replay/Services/DefaultAssemblies.cs
+++ Replay/Services/DefaultAssemblies.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Loader;
 
 namespace Replay.Services
 {
@@ -17,6 +19,7 @@ namespace Replay.Services
 
                 return assemblies
                     .Split(';', StringSplitOptions.RemoveEmptyEntries)
+                    .Where(dll => Path.GetFileName(dll).StartsWith("System"))
                     .Select(path => MetadataReference.CreateFromFile(path))
                     .ToArray();
             });
