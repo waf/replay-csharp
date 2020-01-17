e9762c9deb101527de46bd1d459f25bba7d8d7ed
Will Fuqua <wafuqua@gmail.com>
2020-01-17 20:51:19 +0700

    add syntax highlighting test

---
+++ Replay.Tests/UI/AvalonSyntaxHighlightTransformerTest.cs
@@ -0,0 +1,52 @@
+﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Rendering;
+using NSubstitute;
+using Replay.Services;
+using Replay.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Replay.Tests.UI
+{
+    public class AvalonSyntaxHighlightTransformerTest : IClassFixture<ReplServicesFixture>
+    {
+        private readonly ReplServices replServices;
+        private readonly AvalonSyntaxHighlightTransformer transformer;
+
+        public AvalonSyntaxHighlightTransformerTest(ReplServicesFixture replServicesFixture)
+        {
+            this.replServices = replServicesFixture.ReplServices;
+            this.transformer = new AvalonSyntaxHighlightTransformer(replServices, Guid.Empty);
+        }
+
+        [WpfFact]
+        public void Highlight()
+        {
+            var editor = new TextEditor { Document = new TextDocument() };
+            editor.TextArea.TextView.LineTransformers.Add(
+                transformer
+            );
+            editor.Document.Text = @"var x = ""Hello World""";
+            editor.TextArea.TextView.EnsureVisualLines();
+            var line = editor.Document.Lines.First();
+
+            var visualLine = editor.TextArea.TextView.GetOrConstructVisualLine(line);
+
+            var expectedTokens = new[] { "var", " ", "x", " ", "=", " ", @"""Hello World""" };
+
+            Assert.Equal(
+                expectedTokens.Select(token => token.Length),
+                visualLine.Elements.Select(elem => elem.VisualLength)
+            );
+            Assert.All(
+                visualLine.Elements,
+                element => Assert.NotNull(element.TextRunProperties.ForegroundBrush)
+            );
+                
+        }
+    }
+}
