12674df4d7e8a643ef17cd7fca9bfcc6b4050f76
Will Fuqua <wafuqua@gmail.com>
2020-01-22 23:26:38 +0700

    Add additional intellisense tests

--- Replay.Tests/UI/IntellisenseWindowTest.cs
+++ Replay.Tests/UI/IntellisenseWindowTest.cs
@@ -13,31 +13,61 @@ namespace Replay.Tests.Services
     public class IntellisenseWindowTest : IClassFixture<ReplServicesFixture>
     {
         private readonly ReplServices replServices;
+        private readonly MockKeyboardDevice keyboard;
 
         public IntellisenseWindowTest(ReplServicesFixture replServicesFixture)
         {
             this.replServices = replServicesFixture.ReplServices;
+            this.keyboard = new MockKeyboardDevice();
         }
 
         [WpfFact]
         public async Task IntellisenseWindow_ProvidedCompletions_ShowsCompletions()
         {
-            const string code = "Console.";
-            var completions = await this.replServices.CompleteCodeAsync(Guid.NewGuid(), code, code.Length);
-            Assert.NotEmpty(completions);
+            IntellisenseWindow window = await CompleteText("Console.");
+            PressKey(window, Key.OemPeriod);
 
-            var editor = new TextEditor { Document = new TextDocument() };
-            editor.Document.Text = code;
-
-            var window = new IntellisenseWindow(editor.TextArea, completions, () => { });
-
-            var items = window.CompletionList.ListBox.Items;
-            Assert.NotEmpty(items);
-
-            var first = items[0] as RoslynCompletionSuggestion;
+            var first = GetFirstSuggestion(window);
             Assert.NotNull(first);
             Assert.Equal("BackgroundColor", first.Text);
             Assert.Contains("Gets or sets the background color", first.Description.ToString());
+        }
+
+        [WpfFact]
+        public async Task IntellisenseWindow_AlreadyTypedOnlyCompletion_DoesNotShowWindow()
+        {
+            IntellisenseWindow window = await CompleteText("Console.WriteLine");
+
+            bool closed = false;
+            window.Closed += (_, __) => closed = true;
+
+            PressKey(window, Key.E);
+            Assert.True(closed);
+        }
+
+        private void PressKey(IntellisenseWindow window, Key key)
+        {
+            var keyDown = keyboard.CreateKeyEventArgs(key, Keyboard.KeyDownEvent);
+            window.RaiseEvent(keyDown);
+            var keyUp = keyboard.CreateKeyEventArgs(key, Keyboard.KeyUpEvent);
+            window.RaiseEvent(keyUp);
+        }
+
+        private static RoslynCompletionSuggestion GetFirstSuggestion(IntellisenseWindow window)
+        {
+            var items = window.CompletionList.ListBox.Items;
+            Assert.NotEmpty(items);
+            var first = items[0] as RoslynCompletionSuggestion;
+            return first;
+        }
+
+        private async Task<IntellisenseWindow> CompleteText(string text)
+        {
+            var editor = new TextEditor { Document = new TextDocument() };
+            editor.Document.Text = text;
+            var completions = await this.replServices.CompleteCodeAsync(Guid.NewGuid(), editor.Document.Text, editor.Document.Text.Length);
+            var window = new IntellisenseWindow(editor.TextArea, completions, () => { });
+            return window;
         }
     }
 }
