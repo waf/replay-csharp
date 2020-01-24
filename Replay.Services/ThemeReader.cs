90c015b77722ca899246174102e36b9b6daccb1c
Will Fuqua <wafuqua@gmail.com>
2020-01-24 21:59:27 +0700

    switch from XmlDocument to XmlReader
    less code to maintain this way, as we don't need to worry about the full schema

--- Replay.Services/ThemeReader.cs
+++ Replay.Services/ThemeReader.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Windows.Media;
 using System.Xml;
-using System.Xml.Serialization;
 
 namespace Replay.UI
 {
+    /// <summary>
+    /// Parses a vssettings file (used by Visual Studio) for the contained theme information.
+    /// </summary>
     public static class ThemeReader
     {
         public const string Background = "background";
@@ -16,22 +17,43 @@ namespace Replay.UI
 
         public static IReadOnlyDictionary<string, Color> GetTheme(string filename)
         {
-            var settings = ReadSettings(filename);
             var theme = new Dictionary<string, Color>();
-            foreach (var item in FindColorConfigurations(settings))
+            foreach (var setting in ReadSettings(filename))
             {
-                if (Foreground.Equals(item.Name, StringComparison.InvariantCultureIgnoreCase))
+                if (Foreground.Equals(setting.Name, StringComparison.InvariantCultureIgnoreCase))
                 {
-                    theme[Background] = AbgrToColor(item.Background);
+                    theme[Background] = AbgrToColor(setting.Background);
                 }
-                theme[item.Name.ToLower()] = AbgrToColor(item.Foreground);
+                theme[setting.Name.ToLower()] = AbgrToColor(setting.Foreground);
             }
             return theme;
         }
 
-        private static IEnumerable<Item> FindColorConfigurations(UserSettings settings)
+        private static IReadOnlyCollection<Item> ReadSettings(string filename)
         {
-            return settings.Category.Category.FontsAndColors.Categories.SelectMany(category => category.Items);
+            var list = new List<Item>();
+            using (var stream = new StreamReader(filename))
+            using (var reader = XmlReader.Create(stream))
+            {
+                reader.MoveToContent();
+                reader.ReadToFollowing("FontsAndColors");
+                var fontsAndColors = reader.ReadSubtree();
+                while (fontsAndColors.Read())
+                {
+                    if (fontsAndColors.Name == "Item"
+                        && fontsAndColors.IsStartElement())
+                    {
+                        list.Add(new Item(
+                            background: fontsAndColors.GetAttribute("Background"),
+                            boldFont: fontsAndColors.GetAttribute("BoldFont"),
+                            foreground: fontsAndColors.GetAttribute("Foreground"),
+                            name: fontsAndColors.GetAttribute("Name")
+                        ));
+                    }
+                }
+            }
+
+            return list;
         }
 
         private static Color AbgrToColor(string abgr)
@@ -44,186 +66,20 @@ namespace Replay.UI
             return Color.FromRgb(red, green, blue);
         }
 
-        private static UserSettings ReadSettings(string filename)
+        private readonly struct Item
         {
-            using (var stream = new StreamReader(filename))
-            using (XmlReader reader = XmlReader.Create(stream))
+            public Item(string background, string boldFont, string foreground, string name)
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(UserSettings));
-                return (UserSettings)serializer.Deserialize(reader);
+                Background = background;
+                BoldFont = boldFont;
+                Foreground = foreground;
+                Name = name;
             }
+
+            public string Background { get; }
+            public string BoldFont { get; }
+            public string Foreground { get; }
+            public string Name { get; }
         }
-    }
-
-    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    [XmlRoot(Namespace = "", IsNullable = false)]
-    public partial class UserSettings
-    {
-
-        public ApplicationIdentity ApplicationIdentity { get; set; }
-
-        public ToolsOptions ToolsOptions { get; set; }
-
-        public UserSettingsCategory Category { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class ApplicationIdentity
-    {
-
-        [XmlAttribute]
-        public decimal version { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class ToolsOptions
-    {
-        public ToolsOptionsCategory ToolsOptionsCategory { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class ToolsOptionsCategory
-    {
-
-        [XmlAttribute]
-        public string RegisteredName { get; set; }
-
-        [XmlAttribute]
-        public string name { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class UserSettingsCategory
-    {
-
-        public CategoryCategory Category { get; set; }
-
-        [XmlAttribute]
-        public string RegisteredName { get; set; }
-
-        [XmlAttribute]
-        public string name { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class CategoryCategory
-    {
-
-        public PropertyValue PropertyValue { get; set; }
-
-        public FontsAndColors FontsAndColors { get; set; }
-
-        [XmlAttribute]
-        public string Category { get; set; }
-
-        [XmlAttribute]
-        public string Package { get; set; }
-
-        [XmlAttribute]
-        public string PackageName { get; set; }
-
-        [XmlAttribute]
-        public string RegisteredName { get; set; }
-
-        [XmlAttribute]
-        public string name { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class PropertyValue
-    {
-
-        [XmlAttribute]
-        public string name { get; set; }
-
-        [XmlText]
-        public byte Value { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class FontsAndColors
-    {
-        public FontsAndColorsTheme Theme { get; set; }
-
-        [XmlArrayItem("Category", IsNullable = false)]
-        public FontsAndColorsCategory[] Categories { get; set; }
-
-        [XmlAttribute]
-        public decimal Version { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class FontsAndColorsTheme
-    {
-        [XmlAttribute]
-        public string Id { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class FontsAndColorsCategory
-    {
-        [XmlArrayItem("Item", IsNullable = false)]
-        public Item[] Items { get; set; }
-
-        [XmlAttribute]
-        public byte CharSet { get; set; }
-
-        [XmlIgnore]
-        public bool CharSetSpecified { get; set; }
-
-        [XmlAttribute]
-        public string FontIsDefault { get; set; }
-
-        [XmlAttribute]
-        public string FontName { get; set; }
-
-        [XmlAttribute]
-        public byte FontSize { get; set; }
-
-        [XmlIgnore]
-        public bool FontSizeSpecified { get; set; }
-
-        [XmlAttribute]
-        public string GUID { get; set; }
-    }
-
-    [Serializable]
-    [System.ComponentModel.DesignerCategory("code")]
-    [XmlType(AnonymousType = true)]
-    public partial class Item
-    {
-
-        [XmlAttribute]
-        public string Background { get; set; }
-
-        [XmlAttribute]
-        public string BoldFont { get; set; }
-
-        [XmlAttribute]
-        public string Foreground { get; set; }
-
-        [XmlAttribute]
-        public string Name { get; set; }
     }
 }
